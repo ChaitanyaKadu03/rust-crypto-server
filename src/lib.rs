dba4d562f97103dd89724b94304d7875f00bd4e7
Chaitanya Kadu <chaitanyakadu03@gmail.com>
2024-12-10 21:23:53 +0530

    0.1.0v

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::TcpStream,
+};
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+
+    let _http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rust_clone_server::handle_connection;
+use std::net::TcpListener;
+
 fn main() {
+    let server = TcpListener::bind("127.0.0.1:7878");
+
+    match server {
+        Ok(listener) => {
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(stream) => {
+                        handle_connection(stream);
+                    }
+                    Err(err) => {
+                        println!("The Error is =>>> {err}");
+                    }
+                }
+            }
+        }
+        Err(err) => {
+            println!("The Error is => {err}")
+        }
+    }
     println!("Hello, world!");
 }
